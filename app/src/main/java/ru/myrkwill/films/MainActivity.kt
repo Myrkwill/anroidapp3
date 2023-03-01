af91a94148b7d8f782178e2421df919a5802a040
Mark Nagibin <myrkwill@icloud.com>
2023-03-01 13:53:02 +0500

    Add nav graph

--- app/src/main/java/ru/myrkwill/films/MainActivity.kt
+++ app/src/main/java/ru/myrkwill/films/MainActivity.kt
@@ -2,12 +2,20 @@ package ru.myrkwill.films
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import ru.myrkwill.films.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = Navigation.findNavController(this, R.id.nav_host)
         MAIN = this
     }
 
